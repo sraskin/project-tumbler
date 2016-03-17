6573f88252a493b2a13850008e7ed6de1b2ea5cb
sraskin <shuvho.91@gmail.com>
2016-03-18 03:59:45 +0600

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 # == Route Map
 #
-#    Prefix Verb   URI Pattern               Controller#Action
-#     posts GET    /posts(.:format)          posts#index
-#           POST   /posts(.:format)          posts#create
-#  new_post GET    /posts/new(.:format)      posts#new
-# edit_post GET    /posts/:id/edit(.:format) posts#edit
-#      post GET    /posts/:id(.:format)      posts#show
-#           PATCH  /posts/:id(.:format)      posts#update
-#           PUT    /posts/:id(.:format)      posts#update
-#           DELETE /posts/:id(.:format)      posts#destroy
-#      root GET    /                         posts#index
+#            Prefix Verb   URI Pattern                                 Controller#Action
+#     post_comments GET    /posts/:post_id/comments(.:format)          comments#index
+#                   POST   /posts/:post_id/comments(.:format)          comments#create
+#  new_post_comment GET    /posts/:post_id/comments/new(.:format)      comments#new
+# edit_post_comment GET    /posts/:post_id/comments/:id/edit(.:format) comments#edit
+#      post_comment GET    /posts/:post_id/comments/:id(.:format)      comments#show
+#                   PATCH  /posts/:post_id/comments/:id(.:format)      comments#update
+#                   PUT    /posts/:post_id/comments/:id(.:format)      comments#update
+#                   DELETE /posts/:post_id/comments/:id(.:format)      comments#destroy
+#             posts GET    /posts(.:format)                            posts#index
+#                   POST   /posts(.:format)                            posts#create
+#          new_post GET    /posts/new(.:format)                        posts#new
+#         edit_post GET    /posts/:id/edit(.:format)                   posts#edit
+#              post GET    /posts/:id(.:format)                        posts#show
+#                   PATCH  /posts/:id(.:format)                        posts#update
+#                   PUT    /posts/:id(.:format)                        posts#update
+#                   DELETE /posts/:id(.:format)                        posts#destroy
+#              root GET    /                                           posts#index
+#             about GET    /about(.:format)                            pages#about
 #
 
 Rails.application.routes.draw do
@@ -17,5 +26,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   root 'posts#index'
+
+  get 'about', to: 'pages#about'
 
 end
