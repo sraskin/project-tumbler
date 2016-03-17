158b815ce968c073f2453e1a3d40beb4e13f5876
sraskin <shuvho.91@gmail.com>
2016-03-18 02:53:49 +0600

    Post controller find_post added and Edit, Delete added from Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all.order('created_at DESC')
@@ -9,11 +10,11 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    #@post = Post.find(params[:id])
   end
 
   def edit
-
+    #@post = Post.find(params[:id])
   end
 
   def create
@@ -26,14 +27,23 @@ class PostsController < ApplicationController
   end
 
   def update
-
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @post.destroy
+    redirect_to root_path
   end
 
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :body)
